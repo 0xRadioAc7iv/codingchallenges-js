0bbfe6b5a06aeb0106662cc986585361c39fe31d
Manav <manav18gadhiya@gmail.com>
2024-12-03 15:14:45 +0530

    add: option -l for printing number of lines in a file

--- bin/index.js
+++ bin/index.js
@@ -10,17 +10,39 @@ program
   .description(
     `Print newline, word, and byte counts for each FILE, and a total line if
 more than one FILE is specified.  A word is a non-zero-length sequence of
-printable characters delimited by white space.
-    
-With no FILE, or when FILE is -, read standard input.`
+printable characters delimited by white space.`
   )
   .version("1.0.0");
 
 program
-  .option("-c, --bytes <file_path>", "print the byte counts")
-  .action(async (option) => {
-    const fileBuffer = await fs.readFile(option.bytes);
-    console.log(`${fileBuffer.length} ${option.bytes}`);
+  .argument("[file]", "Path to the file")
+  .option("-c, --bytes", "print the byte counts")
+  .option("-l, --lines", "print the newline counts")
+  .action(async (file, options) => {
+    if (options && Object.keys(options).length > 0) {
+      const bytesOption = options.bytes;
+      const linesOption = options.lines;
+
+      if (file) {
+        if (bytesOption) {
+          const fileBuffer = await fs.readFile(file);
+          console.log(`${fileBuffer.length} ${file}`);
+        }
+
+        if (linesOption) {
+          const fileHandle = await fs.open(file);
+          let lines = 0;
+
+          for await (const line of fileHandle.readLines()) {
+            lines++;
+          }
+
+          console.log(`${lines} ${file}`);
+        }
+      } else {
+        console.error("file: argument not found");
+      }
+    }
   });
 
 program.parse();
